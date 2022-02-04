47c9ecc673eed05a27c7c88d6fdec66be0acd8f0
GerardVivancos <gerard.vivancos@gmail.com>
2022-02-04 01:42:15 +0100

    Fix and enable validation layers

--- README.md
+++ README.md
@@ -2,6 +2,3 @@
 
 ## Known issues
 
-1. In debug mode (`NDEBUG = false`) validation layers are used. This sometimes segfaults but eventually works.
-    - ToDo: implement messaging so initialization can properly talk back and see if there are more insights
-    - ToDo: ended up disabling validation layers until this is solved
--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ const uint32_t HEIGHT = 600;
 #ifdef NDEBUG
     const bool enableValidationLayers = false;
 #else
-    const bool enableValidationLayers = false;
+    const bool enableValidationLayers = true;
 #endif
 
 class VulkanApp {
@@ -29,6 +29,17 @@ private:
     GLFWwindow *window;
     VkInstance instance = VK_NULL_HANDLE;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData) {
+
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+        return VK_FALSE;
+    }
     
     void createWindow() {
         glfwInit();
@@ -59,6 +70,14 @@ private:
         }
     }
 
+    void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo) {
+        createInfo = {};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+    }
+
     void createInstance() {
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -77,11 +96,12 @@ private:
         createInfo.ppEnabledExtensionNames = glfwExtensions;
         createInfo.enabledLayerCount = 0;
 
+        std::vector<const char*> validationLayerNames = {
+            "VK_LAYER_KHRONOS_validation"
+        };
+        VkDebugUtilsMessengerCreateInfoEXT debugCreateInfo{};
         if (enableValidationLayers) {
-            std::vector<const char*>  validationLayerNames = {
-                "VK_LAYER_KHRONOS_validation"
-            };
-
+        
             bool allLayersSupported = true;
             cout << validationLayerNames.size() << " validation layers required" << endl;
             for (auto layer: validationLayerNames) {
@@ -101,6 +121,9 @@ private:
             
             createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayerNames.size());
             createInfo.ppEnabledLayerNames = validationLayerNames.data();
+            
+            populateDebugMessengerCreateInfo(debugCreateInfo);
+            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo;
             cout << "Validation layers enabled" << endl;
         }
 
